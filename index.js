6184c7b1c730e2b0d4b66a1235ba8236946a56a1
Raghu Ram Dhigajarla <raghuramdhigajarla@gmail.com>
2022-02-28 23:21:30 +0530

    update

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require('cors')
 const connection = require('./utils/connection')
 app.use(cors())
 app.use(express.json({limit: '50mb'}))
-// app.use('/',(req,res) => {
-//     res.send("<h1>hello</h1>")
-// })
+app.use('/',(req,res) => {
+    res.send("<h1>hello</h1>")
+})
 dotenv.config();
 //connection();
 mongoose.connect(process.env.DATABASE_ACCESS,()=>console.log("Connected to data base"))
